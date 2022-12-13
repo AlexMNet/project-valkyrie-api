7da4f5dc269a733aa526679f792fc3f773627150
Alex Maldonado <66364639+AlexMNet@users.noreply.github.com>
2022-12-12 19:39:29 -0800

    Add scripts to package json

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,11 @@
 "use strict";
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
 }) : (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
@@ -44,6 +48,9 @@ const PORT = parseInt(process.env.PORT, 10);
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
+app.get('/', (request, response) => {
+    response.send('Project Valkyrie!');
+});
 app.get('/api/healthcheck', (request, response) => {
     const data = {
         uptime: process.uptime(),
